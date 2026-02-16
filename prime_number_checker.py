52809f6dd28090bbcf90dc1ac36f218e5e3d8602
Abhishek Sunilkumar prajapati <abhishhekprajapati07@gmail.com>
2026-02-16 21:15:41 +0530

    Add files via upload

---
+++ prime_number_checker.py
@@ -0,0 +1,14 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:     # if divisible -> not prime
+            return False
+    return True
+    
+# Prime Number Analyzer
+number = int(input("Enter a number to check if it's prime or not: "))
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
